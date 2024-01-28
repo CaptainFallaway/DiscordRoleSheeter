23a9d997f0bf904ffd1b67ece5b8610f6abae925
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-01-28 23:19:20 +0100

    Restored to previous testing

--- tests/discord.py
+++ tests/discord.py
@@ -1,55 +1,43 @@
 import json
 import requests
 
+TOKEN = "MTE5NzY0NjYwOTA0ODE1ODMzMA.GZVCNG.ps32lXOUnU0WdcEsfzh8ARYfGjvWEeShFwj0kE"  # "MTE5NzY0NjYwOTA0ODE1ODMzMA.GZVCNG.ps32lXOUnU0WdcEsfzh8ARYfGjvWEeShFwj0kE"  # noqa E501
+GUILD = "341280708377051137"
 
-class DiscordRoleManager:
-    API_URI = "https://discord.com/api/v10"
+API_URI = "https://discord.com/api/v10"
 
-    TOKEN = "MTE5NzY0NjYwOTA0ODE1ODMzMA.GZVCNG.ps32lXOUnU0WdcEsfzh8ARYfGjvWEeShFwj0kE"
-    GUILD = "341280708377051137"
 
-    HEADERS = {
-            'authorization': f"Bot {TOKEN}",
-            'Content-Type': 'application/json'
-        }
+def pprint(obj):
+    print(json.dumps(obj, indent=4))
 
-    def __init__(self, guild_id: str) -> None:
-        self.guild_id = guild_id
 
-    def _dc_req(self, method: str, endpoint: str, json_data: dict | None = None) -> requests.Response:
-        return requests.request(
-            method,
-            f"{self.API_URI}{endpoint}",
-            json=json_data,
-            headers=self.HEADERS
+def dc_req(method, endpoint, json_data: dict | None = None):
+    headers = {
+        'authorization': f"Bot {TOKEN}",  # Bot {TOKEN}
+        'Content-Type': 'application/json'
+    }
+
+    return requests.request(method, f"{API_URI}{endpoint}", json=json_data, headers=headers).json()
+
+
+def print_dc_req(method, endpoint, json_data: dict | None = None):
+    text = json.dumps(
+        dc_req(method, endpoint, json_data),
+        indent=4
         )
-
-    def get_roles(self) -> None:
-        
+    print(text)
 
 
+def get_roles():
+    resp = dc_req("GET", f"/guilds/{GUILD}/roles")
+    return [role for role in resp if not role["managed"] and role['name'] != "@everyone"]
 
 
+# print_dc_req("DELETE", f"/guilds/{GUILD}/members/{659500796307570728}/roles/{1201040561856053318}")
+
+with open("roles.json", "w") as f:
+    json.dump(get_roles(), f, indent=4)
 
 
-
-
-
-
-
-
-
-
-
-
-# def get_roles():
-#     resp = dc_req("GET", f"/guilds/{GUILD}/roles")
-#     return [role for role in resp if not role["managed"] and role['name'] != "@everyone"]
-
-
-# with open("roles.json", "w") as f:
-#     json.dump(get_roles(), f, indent=4)
-
-
-# with open("members.json", "w") as f:
-#     json.dump(dc_req("GET", f"/guilds/{GUILD}/members?limit=1000"), f, indent=4)
+with open("members.json", "w") as f:
+    json.dump(dc_req("GET", f"/guilds/{GUILD}/members?limit=1000"), f, indent=4)
