69fe7c28a3270b55d0e14f0fa81a12b3d83438db
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-01-30 00:10:12 +0100

    Experimenting on running app asynchronously.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import asyncio
 from kivy.app import App
+from kivy.uix.button import Button
+from lib.discordrolemanager import DiscordRoleManager
+
+GUILD = "341280708377051137"
 
 
 class MainApp(App):
-    pass
+    async def build(self):
+        # return a Button() as a root widget
+        return Button(text='hello world')
 
 
 if __name__ == '__main__':
-    MainApp().run()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(MainApp().async_run())
+    # x = DiscordRoleManager(GUILD)
+    # print(x.get_roles())
