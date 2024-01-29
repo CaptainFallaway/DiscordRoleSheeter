ea36d56c67743891e6bacd5bb47d50b6802c32cf
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-01-30 00:09:12 +0100

    feat: get_roles works

--- lib/discordrolemanager.py
+++ lib/discordrolemanager.py
@@ -1,12 +1,12 @@
 import json  # noqa E402
 import requests
+from .dataclasses import Member, Role, User
 
 
 class DiscordRoleManager:
     API_URI = "https://discord.com/api/v10"
 
     TOKEN = "MTE5NzY0NjYwOTA0ODE1ODMzMA.GZVCNG.ps32lXOUnU0WdcEsfzh8ARYfGjvWEeShFwj0kE"
-    GUILD = "341280708377051137"
 
     HEADERS = {
             'authorization': f"Bot {TOKEN}",
@@ -16,7 +16,7 @@ class DiscordRoleManager:
     def __init__(self, guild_id: str) -> None:
         self.guild_id = guild_id
 
-    def _dc_req(self, method: str, endpoint: str, json_data: dict | None = None) -> requests.Response:
+    async def _dc_req(self, method: str, endpoint: str, json_data: dict | None = None) -> requests.Response:
         return requests.request(
             method,
             f"{self.API_URI}{endpoint}",
@@ -24,5 +24,11 @@ class DiscordRoleManager:
             headers=self.HEADERS
         )
 
-    def get_roles(self) -> None:
-        ...
+    async def get_roles(self) -> list[Role]:
+        resp = self._dc_req("GET", f"/guilds/{self.guild_id}/roles")
+
+        if resp.ok:
+            return [Role(**role) for role in resp.json() if not role["managed"] and role['name'] != "@everyone"]
+        else:
+            # TODO handle error
+            ...
