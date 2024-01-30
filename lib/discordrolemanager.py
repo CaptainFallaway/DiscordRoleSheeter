1b1ffc710d6b6dca405dec7dd2afe661574d2c63
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-01-30 23:50:02 +0100

    fix & feat: added a new response model, fixed the fetching

--- lib/discordrolemanager.py
+++ lib/discordrolemanager.py
@@ -1,6 +1,5 @@
-import json  # noqa E402
-from .dataclasses import Member, Role, User  # noqa E402
-from aiohttp import ClientSession, ClientResponse
+from aiohttp import ClientSession
+from .dataclasses import Member, Role, Resp
 
 
 class DiscordRoleManager:
@@ -16,33 +15,31 @@ class DiscordRoleManager:
     def __init__(self, guild_id: str) -> None:
         self.guild_id = guild_id
 
-    async def _dc_req(self, method: str, endpoint: str, json_data: dict | None = None) -> ClientResponse:
+    async def _dc_req(self, method: str, endpoint: str, json_data: dict | None = None) -> Resp:
         async with ClientSession() as session:
             async with session.get(f"{self.API_URI}{endpoint}", json=json_data, headers=self.HEADERS) as resp:
-                return resp
-
-        # return requests.request(
-        #     method,
-        #     f"{self.API_URI}{endpoint}",
-        #     json=json_data,
-        #     headers=self.HEADERS
-        # )
+                json_data = await resp.json()
+                return Resp(ok=resp.ok, status=resp.status, json=json_data)
 
     async def get_roles(self) -> list[Role]:
         resp = await self._dc_req("GET", f"/guilds/{self.guild_id}/roles")
 
         if resp.ok:
-            return [Role(**role) for role in resp.json() if not role["managed"] and role['name'] != "@everyone"]
+            return [Role(**role) for role in resp.json if not role["managed"] and role['name'] != "@everyone"]
         else:
             # TODO handle error
             ...
 
     async def get_members(self) -> list[Member]:
         resp = await self._dc_req("GET", f"/guilds/{self.guild_id}/members?limit=1000")
-        print(json.dumps(resp.json(), indent=4))
 
         if resp.ok:
-            return [Member(**member) for member in resp.json()]
+            return [Member(**member) for member in resp.json]
         else:
             # TODO handle error
             ...
+
+    # TODO add more methods for setting the roles to the members of the guild.
+    # Lower the ammount of requests to discord api by only changing those who need to be changed.
+    # Aka the ones who changed when the last request was made.
+    # Either you save the previous response or you fetch again.
