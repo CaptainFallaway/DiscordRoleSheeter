02f18c24a67e4070cc5f7fab8c902c1abbf4c57d
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-02-01 23:42:35 +0100

    fix: added all constants here

--- src/helpers/constants.py
+++ src/helpers/constants.py
@@ -1,4 +1,17 @@
+
+# App Window Constants
+
 WINDOW_SIZE = (700, 250)
-DISCORD_GUILD = "341280708377051137"
 ICON_PATH = "src/assets/icon.ico"
 WINDOW_TITLE = "Discord Role Manager"
+
+
+# Discord API Constants
+
+API_URI = "https://discord.com/api/v10"
+TOKEN = "MTE5NzY0NjYwOTA0ODE1ODMzMA.GZVCNG.ps32lXOUnU0WdcEsfzh8ARYfGjvWEeShFwj0kE"
+HEADERS = {
+        'authorization': f"Bot {TOKEN}",
+        'Content-Type': 'application/json'
+    }
+GUILD = "341280708377051137"
