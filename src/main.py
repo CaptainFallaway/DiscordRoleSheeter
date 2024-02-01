c85b3facc10da0283c6a7628cffcc8309d44825a
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-02-01 23:53:53 +0100

    Made mvp pattern truthfully

--- src/main.py
+++ src/main.py
@@ -1,64 +1,39 @@
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.config import Config
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-
 import asyncio
-from typing import Any, Coroutine
-from lib.discordrolemanager import DiscordRoleManager  # Model
 
-GUILD = "341280708377051137"
+from kivy.app import App
+from kivy.config import Config
+from kivy.core.window import Window
+
+from view import View
+from presenter import Presenter
+from helpers.constants import WINDOW_SIZE, ICON_PATH, WINDOW_TITLE
 
 
-class RootWidget(Widget):
-    """Root widget for the app
-
-    This is a Presenter in the MVP pattern.
-    """
-
-    ASPECT_RATIO = 14 / 5  # TODO Imlpement this
+class MainApp(App):
+    """Main app class"""
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.drm = DiscordRoleManager(GUILD)
+        Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+        Config.set('graphics', 'transparent_color', (0, 0, 0, 0))
 
-    def async_func(func: Coroutine) -> None:
-        """Decorator for running async functions in a sync context"""
+        Window.size = WINDOW_SIZE
+        Window.bind(on_resize=lambda *_: Window._set_size(WINDOW_SIZE))  # Static window size
 
-        def wrapper(instance: Any, *args, **kwargs) -> None:
-            loop = asyncio.get_event_loop()
-            loop.create_task(func(instance, *args, **kwargs))
-
-        return wrapper
-
-    @async_func
-    async def on_btn_press(self, instance: Button) -> None:
-        roles = await self.drm.get_roles()
-        print(roles)
-
-    @async_func
-    async def on_btn_press_2(self, instance: Button) -> None:
-        members = await self.drm.get_members()
-        print(members)
-
-
-class MainApp(App):
-    def build(self) -> RootWidget:
-        self.title = "Discord Role Manager"
-        return RootWidget()
+    def build(self) -> View:
+        self.load_kv("kv/main.kv")
+        self.icon = ICON_PATH
+        self.title = WINDOW_TITLE
+        presenter = Presenter()
+        return presenter.view
 
 
 if __name__ == '__main__':
-    Config.set('graphics', 'width', '700')
-    Config.set('graphics', 'height', '250')
-
-    app = MainApp()
-    Builder.load_file("kv/main.kv")
-
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+
+    app = MainApp()
 
     try:
         loop.run_until_complete(app.async_run())
