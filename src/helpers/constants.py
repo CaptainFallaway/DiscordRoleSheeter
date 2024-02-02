65751bb8cdf2c09f963602bd221d6abe6d70adc3
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-02-02 23:11:51 +0100

    Added gateway uri constant

--- src/helpers/constants.py
+++ src/helpers/constants.py
@@ -8,13 +8,15 @@ WINDOW_TITLE = "Discord Role Manager - By captainfallaway"
 
 # Discord API Constants
 
-API_URI = "https://discord.com/api/v10"
 TOKEN = "MTE5NzY0NjYwOTA0ODE1ODMzMA.GZVCNG.ps32lXOUnU0WdcEsfzh8ARYfGjvWEeShFwj0kE"
+GUILD = "341280708377051137"
+
+API_URI = "https://discord.com/api/v10"
+GATEWAY_URI = "wss://gateway.discord.gg/?v=10&encoding=json"
 HEADERS = {
         'authorization': f"Bot {TOKEN}",
         'Content-Type': 'application/json'
     }
-GUILD = "341280708377051137"
 
 
 # Kivy Lang Path Constants
